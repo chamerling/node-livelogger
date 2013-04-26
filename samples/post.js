5db5e4deb7fbef84d792769c02e68ac53fecee14
Christophe Hamerling <christophe.hamerling@gmail.com>
2013-04-26 17:09:38 +0200

    randomize

--- samples/post.js
+++ samples/post.js
@@ -7,6 +7,22 @@
 var request = require('request')
   , conf = require('../config');
 
+var levels = ['fine', 'info', 'success', 'warning', 'error'];
+var messages = [
+  'This is a message A',
+  'This is a message B',
+  'This is a message C',
+  'This is a message D',
+  'This is a message E',
+  'This is a message F',
+  'This is a message G',
+  'This is a message H',
+  'This is a message I',
+  'This is a message J',
+  'This is a message K',
+  'This is a message L'
+]
+
 var payload = {
 
   // minimal message
@@ -21,6 +37,9 @@ var payload = {
 
 var send = function() {
   payload.date = new Date()
+  payload.message = messages[Math.floor(Math.random() * (messages.length))]
+  payload.level = levels[Math.floor(Math.random() * (levels.length))]
+
   request(
     {
       method : 'post',
